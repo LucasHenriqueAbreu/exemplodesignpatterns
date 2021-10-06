441e2d29963764f4948954dbd36dfb5a71017ef0
Lucas Henrique de Abreu <lucasigual14@gmail.com>
2021-10-05 22:24:17 -0300

    correções no equals do Circulo

--- src/prototype/Circulo.java
+++ src/prototype/Circulo.java
@@ -24,9 +24,6 @@ public class Circulo extends Forma{
     public boolean equals(Object obj) {
         if (!(obj instanceof Circulo) || (!super.equals(obj))) return false;
         Circulo circulo2 = (Circulo) obj;
-        return circulo2.x == this.x &&
-                circulo2.y == this.y &&
-                Objects.equals(circulo2.cor, this.cor) &&
-                circulo2.raio == this.raio;
+        return circulo2.raio == this.raio;
     }
 }
